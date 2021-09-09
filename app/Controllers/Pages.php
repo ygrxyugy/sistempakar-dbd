76fce95b78855701b271fc70f333f31fed5c4fd5
adillashidiq <adill.ashidiq20@gmail.com>
2021-09-09 16:04:54 +0700

    Membuat controller pages

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('pages/index');
+    }
+
+    public function login()
+    {
+        return view('pages/login');
+    }
+}
