7547a16421e4e008c780369454f049688919f805
adillashidiq <adill.ashidiq20@gmail.com>
2021-09-21 22:27:33 +0700

    merapihkan footer dan topbar untuk halaman index

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,39 +1,47 @@
 <div class="col-12 bg-gradient-primary" style="width: 100%;">
     <nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary ">
         <div class="container">
-            <a href="/"><i class="fas fa-cloud-meatball navbar-brand"> HeartBreak</i></a> 
+            <a href="/"><i class="fas fa-cloud-meatball navbar-brand"> HeartBreak</i></a>
             <ul class="navbar-nav ml-auto float-right">
                 <li class="nav-item">
-                <a class="nav-link" href="/login"><i class="fas fa-sign-in-alt mr-2"> login</i></a>
+                    <a class="nav-link" href="/login"><i class="fas fa-sign-in-alt mr-2"> login</i></a>
                 </li>
             </ul>
-            </div>
         </div>
-    </nav>    
 </div>
-<p>    -------------------------Pengenalan Website---------------------</p>
-<p>    -------------------------Bahaya Penyakit Jantung---------------------</p>
-<p> cek apakah anda menderita penyakit jantung <button class="bg-gradient-primary"><a href="/user/survey" class="text-light text-decoration-none">di sini</a></button> </p>
-
+</nav>
+</div>
+<div class="text-center">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <p> -------------------------Pengenalan Website---------------------</p>
+    <p> -------------------------Bahaya Penyakit Jantung---------------------</p>
+    <p> cek apakah anda menderita penyakit jantung <button class="bg-gradient-primary"><a href="/user/survey" class="text-light text-decoration-none">di sini</a></button> </p>
+</div>
 <!-- Setelah ada konten, tolong hapus div yang berisi <br> di bawah ini -->
-    <div class="col-1">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-    </div>
-
-</body>
+<div class="col-1">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>
