5a1dcc9fd5093d6cb5e943853fc7fdb18066cc6f
adillashidiq <adill.ashidiq20@gmail.com>
2021-10-02 19:42:57 +0700

    merubah title heartbreak to heartnalyze

--- app/Views/templates/sidebar-user.php
+++ app/Views/templates/sidebar-user.php
@@ -2,19 +2,19 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('user');?>">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('user'); ?>">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-cloud-meatball"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3"> HeartBreak </div>
+                <div class="sidebar-brand-text mx-3"> Heartnalyze </div>
             </a>
 
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item <?= ($title === "User Dashboard") ? 'active' : '' ; ?>">
-                <a class="nav-link" href=" <?= base_url('user');?>">
+            <li class="nav-item <?= ($title === "User Dashboard") ? 'active' : ''; ?>">
+                <a class="nav-link" href=" <?= base_url('user'); ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -23,44 +23,44 @@
             <hr class="sidebar-divider">
 
             <!-- Data Pengguna -->
-            <li class="nav-item <?= ($title === "Profile") ? 'active' : '' ; ?>">
-                <a class="nav-link" href=" <?= base_url('user/profile');?>">
+            <li class="nav-item <?= ($title === "Profile") ? 'active' : ''; ?>">
+                <a class="nav-link" href=" <?= base_url('user/profile'); ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Profil</span>
                 </a>
             </li>
             <!-- Divider -->
-            
-            <hr class="sidebar-divider">            
+
+            <hr class="sidebar-divider">
             <!-- Data Pengguna -->
 
-            <li class="nav-item <?= ($title === "Cek Kesehatan") ? 'active' : '' ; ?>">
-                <a class="nav-link" href=" <?= base_url('user/survey');?>">
+            <li class="nav-item <?= ($title === "Cek Kesehatan") ? 'active' : ''; ?>">
+                <a class="nav-link" href=" <?= base_url('user/survey'); ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Cek Kesehatan</span>
                 </a>
             </li>
-            
+
             <!-- Divider -->
             <hr class="sidebar-divider">
-            
+
             <!-- Data Pengguna -->
-            <li class="nav-item <?= ($title === "Riwayat Pemeriksaan") ? 'active' : '' ; ?>">
-                <a class="nav-link" href=" <?= base_url('user/history');?>">
+            <li class="nav-item <?= ($title === "Riwayat Pemeriksaan") ? 'active' : ''; ?>">
+                <a class="nav-link" href=" <?= base_url('user/history'); ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Riwayat Pemeriksaan</span>
                 </a>
             </li>
-            
+
             <!-- Divider -->
             <hr class="sidebar-divider">
 
             <!-- Logout -->
             <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('user');?>">
-                <i class="fas fa-fw fa-sign-out-alt"></i>
-                <span>Logout</span>
-            </a>
+                <a class="nav-link" href="<?= base_url('user'); ?>">
+                    <i class="fas fa-fw fa-sign-out-alt"></i>
+                    <span>Logout</span>
+                </a>
             </li>
 
             <!-- Divider -->
